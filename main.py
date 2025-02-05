3b393caadb99d08d65e8d807a9c48bcb590fd2a0
postmodernweaboo <raider824477@gmail.com>
2025-02-05 12:50:53 +0900

    Ability to draw player using classes.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+
 
     print(
     "Starting asteroids!\n"
@@ -17,9 +22,10 @@ def main():
                 return
             
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
-
+        
+        dt = Clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
